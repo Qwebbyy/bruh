4a23da6e1afccaceb6e8765848ff066b6b95c36a
Qwebbyy <123127280+Qwebbyy@users.noreply.github.com>
2023-01-19 15:17:39 -0800

    a

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    music.playTone(440, music.beat(BeatFraction.Whole))
+})
+input.onButtonPressed(Button.AB, function () {
+    music.playTone(392, music.beat(BeatFraction.Whole))
+})
+input.onButtonPressed(Button.B, function () {
+    music.playTone(494, music.beat(BeatFraction.Whole))
+})
+input.onGesture(Gesture.Shake, function () {
+    music.playTone(587, music.beat(BeatFraction.Whole))
 })
